b95cd2d6b3de24f40e19d5fc95a2733f68bcddd7
Premnath08 <59083047+Premnath08@users.noreply.github.com>
2021-01-25 22:27:17 +0530

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,13 @@
+print("Today is a nice day")
+print('Python is fun')
+print('lets learn "python" today')
+print("we can use single 'quote' in a string")
+print("hello"+"world")  #string concatenation
+greeting="hello"
+name="Prem"
+
+# to add a space between strings assigned to a variable
+print(greeting+" "+name)
+label=input("Please enter your name ")
+
+print(greeting+label)
